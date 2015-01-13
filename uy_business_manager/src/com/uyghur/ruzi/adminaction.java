04143476df75bc5ee704b6d4e04768f74083924a
uygurran2008@126.com <uygurjan2008@126.com>
2015-01-13 17:55:39 +0800

    Signed-off-by: uygurran2008@126.com <uygurjan2008@126.com>

--- uy_business_manager/src/com/uyghur/ruzi/adminaction.java
+++ uy_business_manager/src/com/uyghur/ruzi/adminaction.java
@@ -1,5 +1,65 @@
 package com.uyghur.ruzi;
 
-public class adminaction {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.uyghur.ruzi.dao.admindao;
+import com.uyghur.ruzi.dao.goodsdao;
+
+public class adminaction {
+	
+	
+	
+	
+	public String get_all_users() throws IOException{
+		// JSON格式数据解析对象
+				HttpServletResponse response = ServletActionContext.getResponse();
+				
+				PrintWriter out;
+				out = response.getWriter();
+				// 将要被返回到客户端的对象
+
+				
+
+				
+				
+				admindao ad = new admindao();
+				List l1 = ad.users_R();
+
+				JSONArray ja3 = JSONArray.fromObject(l1);
+
+				JSONObject jo=new JSONObject(); 
+
+				// 将Bean转换为JSONArray数据
+
+				System.out.println("JSONArray对象数据格式：");
+
+				System.out.println(ja3.toString());
+
+				// 构造Json数据，包括一个map和一个Employee对象
+
+				// jo.put("AAA", ja3);
+				jo.put("info", "users in data base");
+				jo.put("success", true); 
+				jo.put("items", ja3);
+
+				jo.write(out);
+
+				System.out.println("\n最终构造的JSON数据格式：");
+				System.out.println(jo.toString());
+
+				out.flush();
+				out.close();
+
+				return "success";// jo;
+		
+	}	
 }
--- uy_business_manager/src/com/uyghur/ruzi/dao/admindao.java
+++ uy_business_manager/src/com/uyghur/ruzi/dao/admindao.java
@@ -1,5 +1,123 @@
 package com.uyghur.ruzi.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import db.Dbdriver;
+
 public class admindao {
+
+	private String username;
+	private String userpassword;
+	private String useremail;
+	private int userid;
+	private int tel;
+	private String loginip;
+	private String gender;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUserpassword() {
+		return userpassword;
+	}
+
+	public void setUserpassword(String userpassword) {
+		this.userpassword = userpassword;
+	}
+
+	public String getUseremail() {
+		return useremail;
+	}
+
+	public void setUseremail(String useremail) {
+		this.useremail = useremail;
+	}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public int getTel() {
+		return tel;
+	}
+
+	public void setTel(int tel) {
+		this.tel = tel;
+	}
+
+	public String getLoginip() {
+		return loginip;
+	}
+
+	public void setLoginip(String loginip) {
+		this.loginip = loginip;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "admindao [username=" + username + ", userpassword="
+				+ userpassword + ", useremail=" + useremail + ", userid="
+				+ userid + ", tel=" + tel + ", loginip=" + loginip
+				+ ", gender=" + gender + "]";
+	}
+
+	public List<admindao> users_R() {
+
+		List<admindao> users = new ArrayList<admindao>();
+		Dbdriver db = new Dbdriver();
+
+		try {
+			Class.forName(db.driver());
+			Connection conn = db.getConn();
+			Statement stmt = conn.createStatement();
+			String sql = "SELECT * FROM `users`";
+			ResultSet rs = stmt.executeQuery(sql);
+			System.out.println("sql" + sql);
+
+			while (rs.next()) {
+
+				admindao gd = new admindao();
+				gd.setUsername(rs.getString("username"));
+				gd.setUserpassword(rs.getString("userpassword"));
+				gd.setUseremail(rs.getString("useremail"));
+				gd.setUserid(rs.getInt("userid"));
+				gd.setTel(rs.getInt("tel"));
+				gd.setLoginip(rs.getString("loginip"));
+				gd.setGender(rs.getString("gender"));
+
+				users.add(gd);
+
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return users;
+
+	}
 
 }
